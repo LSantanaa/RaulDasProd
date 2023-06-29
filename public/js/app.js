5fb0c6b5310a599765a335d29c19dc6a4be80ac2
Leonardo <leonardodesantana05@gmail.com>
2023-06-29 20:06:19 -0300

    isolando js com IIFE

--- public/js/app.js
+++ public/js/app.js
@@ -1,140 +1,142 @@
 /*Outside Click Function*/
-function outsideClick(element, events, callback) {
-  const html = document.documentElement;
-  const outside = "data-outside";
+(function() {
+  function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = "data-outside";
 
-  if (!element.hasAttribute(outside)) {
-    events.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutside);
-    });
-
-    element.setAttribute(outside, "");
-  }
-  function handleOutside(event) {
-    if (!element.contains(event.target)) {
-      element.removeAttribute(outside);
+    if (!element.hasAttribute(outside)) {
       events.forEach((userEvent) => {
-        html.removeEventListener(userEvent, handleOutside);
+        html.addEventListener(userEvent, handleOutside);
       });
-      callback();
+
+      element.setAttribute(outside, "");
     }
-  }
-}
-/**BTN MOBILE FUNCTION */
-
-function toggleMenu() {
-  const btn = document.querySelector(".btn__mobile");
-  const nav = document.querySelector(".header__nav");
-  const ativo = "ativo";
-  const eventos = ["click", "touchstart"];
-
-  eventos.forEach((e) => {
-    btn.addEventListener(e, handleClick, { passive: false });
-  });
-
-  function handleClick(event) {
-    if (event.type === "touchstart" && event.cancelable) event.preventDefault();
-    nav.classList.toggle(ativo);
-    const active = nav.classList.contains(ativo);
-    event.currentTarget.setAttribute("aria-expanded", active);
-
-    if (active) {
-      event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-    }
-
-    outsideClick(this, eventos, () => {
-      nav.classList.remove(ativo);
-    });
-  }
-}
-toggleMenu();
-
-/**Hidden Iframe Youtube */
-function lazyObserver() {
-  function loadVideo(iframe) {
-    const src = iframe.getAttribute("data-src");
-    iframe.setAttribute("src", src);
-  }
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const iframe = entry.target;
-        loadVideo(iframe);
-        observer.unobserve(iframe);
+    function handleOutside(event) {
+      if (!element.contains(event.target)) {
+        element.removeAttribute(outside);
+        events.forEach((userEvent) => {
+          html.removeEventListener(userEvent, handleOutside);
+        });
+        callback();
       }
+    }
+  }
+  /**BTN MOBILE FUNCTION */
+
+  function toggleMenu() {
+    const btn = document.querySelector(".btn__mobile");
+    const nav = document.querySelector(".header__nav");
+    const ativo = "ativo";
+    const eventos = ["click", "touchstart"];
+
+    eventos.forEach((e) => {
+      btn.addEventListener(e, handleClick, { passive: false });
     });
-  });
 
-  const lazyVideos = document.querySelectorAll(".lazy");
+    function handleClick(event) {
+      if (event.type === "touchstart" && event.cancelable) event.preventDefault();
+      nav.classList.toggle(ativo);
+      const active = nav.classList.contains(ativo);
+      event.currentTarget.setAttribute("aria-expanded", active);
 
-  lazyVideos.forEach((lazyVideo) => {
-    observer.observe(lazyVideo);
-  });
-}
+      if (active) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+      }
 
-lazyObserver();
+      outsideClick(this, eventos, () => {
+        nav.classList.remove(ativo);
+      });
+    }
+  }
+  toggleMenu();
 
-/*Scroll Reveal Animation*/
-function scrollRevealInit() {
-  const sr = ScrollReveal({
-    easing: "ease",
-    duration: 1000,
-    origin: "bottom",
-    distance: "100%",
-  });
+  /**Hidden Iframe Youtube */
+  function lazyObserver() {
+    function loadVideo(iframe) {
+      const src = iframe.getAttribute("data-src");
+      iframe.setAttribute("src", src);
+    }
 
-  sr.reveal(".container__header", {
-    delay: 100,
-  });
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const iframe = entry.target;
+          loadVideo(iframe);
+          observer.unobserve(iframe);
+        }
+      });
+    });
 
-  sr.reveal(".container__logo", {
-    delay: 200,
-  });
+    const lazyVideos = document.querySelectorAll(".lazy");
 
-  sr.reveal(".container__description", {
-    distance: "70%",
-    origin: "left",
-    delay: 200,
-  });
+    lazyVideos.forEach((lazyVideo) => {
+      observer.observe(lazyVideo);
+    });
+  }
 
-  sr.reveal(".container__card", {
-    distance: "70%",
-    origin: "left",
-    delay: 220,
-  });
+  lazyObserver();
 
-  sr.reveal(".container__division, .sobre__container__division", {
-    duration: 1200,
-    delay: 260,
-  });
+  /*Scroll Reveal Animation*/
+  function scrollRevealInit() {
+    const sr = ScrollReveal({
+      easing: "ease",
+      duration: 1000,
+      origin: "bottom",
+      distance: "100%",
+    });
 
-  sr.reveal(".sobre__container__formation", {
-    duration: 1300,
-    delay: 300,
-  });
+    sr.reveal(".container__header", {
+      delay: 100,
+    });
 
-  sr.reveal(".container__carousel", {
-    duration: 1300,
-    delay: 300,
-  });
-}
-scrollRevealInit();
+    sr.reveal(".container__logo", {
+      delay: 200,
+    });
 
-/*Init Carousel*/
+    sr.reveal(".container__description", {
+      distance: "70%",
+      origin: "left",
+      delay: 200,
+    });
 
-const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 5000,
-  },
-  loop: true,
+    sr.reveal(".container__card", {
+      distance: "70%",
+      origin: "left",
+      delay: 220,
+    });
 
-  pagination: {
-    el: ".swiper-pagination",
-  },
+    sr.reveal(".container__division, .sobre__container__division", {
+      duration: 1200,
+      delay: 260,
+    });
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    sr.reveal(".sobre__container__formation", {
+      duration: 1300,
+      delay: 300,
+    });
+
+    sr.reveal(".container__carousel", {
+      duration: 1300,
+      delay: 300,
+    });
+  }
+  scrollRevealInit();
+
+  /*Init Carousel*/
+
+  const swiper = new Swiper(".swiper", {
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    })
+})();
