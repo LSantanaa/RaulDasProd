3b97e80f3d9785fa835546fbd4a4b183c68f7faf
Leonardo <leonardodesantana05@gmail.com>
2023-10-18 08:49:49 -0300

    debugando em produção, loucura? talvez

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -3,9 +3,19 @@ import axios from 'axios';
 
 export const authInsta = (req: Request, res: Response) => {
   res.redirect(`https://api.instagram.com/oauth/authorize?client_id=625643059766168&redirect_uri=https://raul-das-prod.vercel.app/auth&scope=user_profile,user_media&response_type=code`)
+}
 
+export const getAccessToken = async (req: Request, res: Response) => {
   const code = req.query.code;
-  console.log(code)
+  console.log("Código de autorização:", code); // Adicione este log para verificar o código
+
+  try {
+    res.send("Autorização bem-sucedida. Você pode fechar esta janela agora.");
+      // Resto do seu código para obter o token de acesso
+  } catch (error) {
+    console.error(`Erro ao obter o token de acesso do Instagram: ${error}`);
+    res.status(500).send("Erro ao obter o token de acesso do Instagram.");
+  }
 }
 
 // export const getAccessToken = async (req: Request, res: Response) => {
