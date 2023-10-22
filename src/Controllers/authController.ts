84ee02784d13558791a130fd875ec31d811bc487
Leonardo <leonardodesantana05@gmail.com>
2023-10-22 01:32:19 -0300

    test: axios post

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -1,59 +1,37 @@
 import { Request, Response } from "express";
 import axios from 'axios';
+import querystring from 'querystring';
 
 export const authInsta = (req: Request, res: Response) => {
   res.redirect(`https://api.instagram.com/oauth/authorize?client_id=625643059766168&redirect_uri=https://raul-das-prod.vercel.app/auth&scope=user_profile,user_media&response_type=code`)
 }
 
 export const getAccessToken = async (req: Request, res: Response) => {
-  
-  console.log("URL completa:", req.url);
+  const { code } = req.query;
 
-  const regex = /\?code=(.*)(?=#_)/;
-  const match = req.url.match(regex);
-  if (match) {
-    const code = match[1];
-    console.log("Código de autorização:", code);
+  if (!code) {
+    return res.status(400).send('Código de autorização ausente.');
   }
 
   try {
-    res.send("Autorização bem-sucedida. Você pode fechar esta janela agora.");
-      // Resto do seu código para obter o token de acesso
+    const accessTokenResponse = await axios.post(
+      'https://api.instagram.com/oauth/access_token',
+      querystring.stringify({
+        client_id: 625643059766168,
+        client_secret: '7cfce4dbad452f6a1a9be6b46cd3ac8f',
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://raul-das-prod.vercel.app/auth',
+        code: code.toString()
+      })
+    );
+
+    const accessToken = accessTokenResponse.data.access_token;
+    
+    res.send('Token de acesso obtido com sucesso: ' + accessToken);
   } catch (error) {
-    console.error(`Erro ao obter o token de acesso do Instagram: ${error}`);
-    res.status(500).send("Erro ao obter o token de acesso do Instagram.");
+    console.error('Erro ao obter token de acesso:', error);
+    res.status(500).send('Erro ao obter token de acesso.');
   }
-}
-
-// export const getAccessToken = async (req: Request, res: Response) => {
-//     res.redirect()
-//     // try {
-//     //   const response = await axios({
-//     //     method: 'post',
-//     //     url: 'https://api.instagram.com/oauth/access_token',
-//     //     headers: {
-//     //       'Accept': 'application/json',
-//     //       'Content-Type': 'application/x-www-form-urlencoded'
-//     //     },
-//     //     data: {
-//     //       client_id: '625643059766168',
-//     //       client_secret: '7cfce4dbad452f6a1a9be6b46cd3ac8f',
-//     //       grant_type: 'authorization_code',
-//     //       redirect_uri: 'https://raul-das-prod.vercel.app/auth',
-//     //       code,
-//     //     }
-//     //   });
-
-//     //   const accessToken = response.data.access_token;
-//     //   localStorage.setItem('token', JSON.stringify(accessToken))
-//     //   console.log(accessToken)
-//     //   res.redirect('/')
-//     // } catch (error) {
-//     //   res.redirect('/')
-//     //   console.error(`Erro ao obter o token de acesso do Instagram: ${error}`);
-//     // }
-//}
-
-
+};
 
 
