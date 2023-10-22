e951447ef8c58cdf063e7aeed38b4a009cbc8585
Leonardo <leonardodesantana05@gmail.com>
2023-10-22 03:28:21 -0300

    test: tentativa de corrigir o axios

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import axios from 'axios';
 import querystring from 'querystring';
 
@@ -13,11 +13,8 @@ export const getAccessToken = async (req: Request, res: Response) => {
     return res.status(400).send('Código de autorização ausente.');
   }
 
-  let accessToken: any;
-  let longAccessToken:any;
-
   try {
-     await axios.post(
+    const accessTokenResponse = await axios.post(
       'https://api.instagram.com/oauth/access_token',
       querystring.stringify({
         client_id: 625643059766168,
@@ -26,23 +23,23 @@ export const getAccessToken = async (req: Request, res: Response) => {
         redirect_uri: 'https://raul-das-prod.vercel.app/auth',
         code: code.toString()
       })
-    ).then((response)=>{
-      console.log(response.data)
-      accessToken = response.data.access_token;
-      return axios.get('https://graph.instagram.api/access_token',{
-        params:{
-          grant_type:'ig_exchange_token',
-          client_secret: '7cfce4dbad452f6a1a9be6b46cd3ac8f',
-          access_token: accessToken
-        }
-      }).then((response)=>{
-        longAccessToken = response.data.access_token;
-      })
+    );
+    let accessToken = accessTokenResponse.data.access_token;
+
+    const longAccessTokenResponse = await axios.get('https://graph.instagram.com/access_token', {
+      params: {
+        grant_type: 'ig_exchange_token',
+        client_secret: '7cfce4dbad452f6a1a9be6b46cd3ac8f',
+        access_token: accessToken
+      }
     })
-    
-    res.send('Token de acesso curto obtido com sucesso: ' + accessToken);
-    res.send('Token de acesso longo obtido com sucesso: ' + longAccessToken.access_token);
-    res.send('Token de acesso longo expira em: ' + (((longAccessToken.expires_in / 60) / 60) / 24 ).toFixed(0) + 'Dias');
+
+    let longAccessToken = longAccessTokenResponse.data;
+
+    res.send(`Token de acesso curto obtido com sucesso: ${accessToken}<br>
+    Token de acesso longo obtido com sucesso: ${longAccessToken.access_token}<br>
+    O Token expira em ${(((longAccessToken.expires_in / 60) / 60) / 24).toFixed(0)} dias.
+    ` );
   } catch (error) {
     console.error('Erro ao obter token de acesso:', error);
     res.status(500).send('Erro ao obter token de acesso.');
