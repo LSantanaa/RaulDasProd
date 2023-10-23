bff186df07b10cd60f83df69434c30b8235e73e9
Leonardo <leonardodesantana05@gmail.com>
2023-10-23 08:57:10 -0300

    test: salvando token e mida do usuario no banco

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -3,13 +3,16 @@ import axios from 'axios';
 import querystring from 'querystring';
 import dotenv from 'dotenv';
 import { TokenModel } from "../Model/Schema/tokenSchema";
+import { userMedia } from "../Model/Schema/userMediaSchema";
 
 dotenv.config();
 
+//ao acessar a rota valida_insta redireciona para receber a permissão do usuário
 export const authInsta = (req: Request, res: Response) => {
   res.redirect(`https://api.instagram.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=user_profile,user_media&response_type=code`)
 }
 
+//se a confirmação for feita, o código de acesso é enviado via parametro na url da rota /auth, com o código faço a requisição de um token curto e troco por um token de longo acesso
 export const getAccessToken = async (req: Request, res: Response) => {
   const { code } = req.query;
 
@@ -18,6 +21,7 @@ export const getAccessToken = async (req: Request, res: Response) => {
   }
 
   try {
+    //token curto
     const accessTokenResponse = await axios.post(
       'https://api.instagram.com/oauth/access_token',
       querystring.stringify({
@@ -30,6 +34,7 @@ export const getAccessToken = async (req: Request, res: Response) => {
     );
     let accessToken = accessTokenResponse.data.access_token;
 
+    //token longo
     const longAccessTokenResponse = await axios.get('https://graph.instagram.com/access_token', {
       params: {
         grant_type: 'ig_exchange_token',
@@ -37,28 +42,42 @@ export const getAccessToken = async (req: Request, res: Response) => {
         access_token: accessToken
       }
     })
-
     let longAccessToken = longAccessTokenResponse.data;
 
+    
+    const getUserInsta = await axios.get(`https://graph.instagram.com/me?access_token${longAccessToken.access_token}`)
+    const getUserMedia = await axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_url,is_shared_to_feed,media_type,thumbnail_url&access_token=${longAccessToken.access_token}`)
+    
+    const username = getUserInsta.data.username;
+    const data = getUserMedia.data.data
+    
+    //inserir media no banco
+    const newUserMedia = new userMedia({
+      username,
+      data
+    })
 
+    const inserirMedia = await newUserMedia.save();
 
+    //inserir token no banco de dados
     const today = new Date();
     const secondsToExpire = longAccessToken.expires_in;
     const expirationDate = new Date(today.getTime() + secondsToExpire * 1000)
     
     const newToken = new TokenModel({
-      user: 'teste1',
-      longToken: 'tokenTest',
+      user: username,
+      longToken: longAccessToken.access_token,
       createdAt: today,
       expiresAt: expirationDate
     })
-
     const inserirToken = await newToken.save();
 
-    res.send(`Token de acesso curto obtido com sucesso: ${accessToken}<br>
-    Token de acesso longo obtido com sucesso: ${longAccessToken.access_token}<br>
-    O Token expira em ${(((longAccessToken.expires_in / 60) / 60) / 24).toFixed(0)} dias.
-    ` );
+    if(inserirMedia && inserirToken){
+      res.redirect('/')
+    }else{
+      res.send('Houve um erro ao salvar os dados, contate o administrador.')
+    }
+   
   } catch (error) {
     console.error('Erro ao obter token de acesso:', error);
     res.status(500).send('Erro ao obter token de acesso.');
---
+++ src/Model/Schema/userMediaSchema.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export interface userMedia{
+  username: string,
+  data:[
+    {
+      id: number,
+      caption: string,
+      media_url: string,
+      media_type: string,
+      thumbnail_url: string,
+      is_shared_to_feed: boolean
+    }
+  ]
+}
+
+const userSchema = new mongoose.Schema<userMedia>({
+  username: String,
+  data:[
+    {
+      id: Number,
+      caption: String,
+      media_url: String,
+      media_type: String,
+      thumbnail_url: String,
+      is_shared_to_feed: Boolean
+    }
+  ]
+});
+
+export const userMedia = mongoose.model<userMedia>('Token', userSchema);
