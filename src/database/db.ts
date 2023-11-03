efee4940779ff35cb2603bea3b5889b0402810ff
Leonardo <leonardodesantana05@gmail.com>
2023-11-02 22:48:59 -0300

    Style: mudança no background e cores do menu

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const mongoConnect = async ()=>{
- await mongoose.connect(`${process.env.MONGODB_URI}`)
+ await mongoose.connect(`${process.env.MONGOLOCAL}`)
   
   const db =  mongoose.connection;
   
